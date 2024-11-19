21724ea10ec8345f087b6cd68708f11a3fd0cb3f
Arnab <arnabpramanik102018@gmail.com>
2024-11-19 16:18:38 +0530

    search

--- app/Main.jsx
+++ app/Main.jsx
@@ -10,6 +10,7 @@ function Main() {
   const [items, setItems] = useState([]); // State to store fetched items
   const [loading, setLoading] = useState(true); // State to handle loading state
   const [error, setError] = useState(null); // State to handle errors
+  const[search,setSearch]=useState('')  //State to handle search
 
   useEffect(() => {
     // Fetch data when the component mounts
@@ -56,9 +57,25 @@ function Main() {
         <div className="fixed top-0 z-[99999] w-full mb-8">
           <Navbar />
         </div>
-
+        
         <div className="p-4 mt-16">
           {/* Show loading or error state if data is still being fetched */}
+          <div className="mb-4">
+          
+<form class="max-w-md mx-auto">   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search News..." onChange={(e) => setSearch(e.target.value)} />
+        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+    </div>
+</form>
+
+        </div>
           {loading ? (
             <div className="text-white text-center">Loading...</div>
           ) : error ? (
@@ -66,7 +83,8 @@ function Main() {
           ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-6">
                 {/* {console.log(items)} */}
-              {items.map((item, index) => (
+              {items.filter((item)=>{return search.toLowerCase()===''?item: item.title.toLowerCase().includes(search.toLowerCase())
+            }).map((item, index) => (
                 <div key={index} className="flex justify-center">
                    {/* { console.log(item.link)}
                    {console.log(item.title)} */}
